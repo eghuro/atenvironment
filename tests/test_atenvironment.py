e17cb80b98518d0a9e42a1404b21a61a5e65fdef
Alexandr Mansurov <alex@eghuro.cz>
2018-06-12 02:34:42 +0200

    Dump CLI

--- tests/test_atenvironment.py
+++ tests/test_atenvironment.py
@@ -5,10 +5,8 @@
 
 
 import unittest
-from click.testing import CliRunner
 
 from atenvironment import atenvironment
-from atenvironment import cli
 
 
 class TestAtenvironment(unittest.TestCase):
@@ -22,13 +20,3 @@ class TestAtenvironment(unittest.TestCase):
 
     def test_000_something(self):
         """Test something."""
-
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'atenvironment.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
